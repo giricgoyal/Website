25bdc62af9479d79bdd3661e867dc021fa7b04c1
anshika <aagarw6@uic.edu>
2014-09-07 20:36:21 -0500

    updating art and artist
    combining with the php

--- artnartists/artist/index.php
+++ artnartists/artist/index.php
@@ -1,10 +1,10 @@
 <?php
-	include "include/variables.php";
+	include "../include/variables.php";
 	include $SITE_INCLUDE . "/session.php";
 	
 
 	include $SITE_FORMS . "/header.html";
-	include $SITE_FORMS . "/art.html";
+	include $SITE_ARTIST_FORMS . "/artist.html";
 	include $SITE_FORMS . "/footer.html";
 ?>
 
--- artnartists/include/variables.php
+++ artnartists/include/variables.php
@@ -22,15 +22,28 @@
 	$SITE_ADMIN_INCLUDE		=	"include";
 	$SITE_ADMIN_FORMS		=	"forms";
 
+	$SITE_ARTIST_BASE	=	$SITE_BASE . "/artist";
+	$SITE_ARTIST_FORMS	=	$SITE_ARTIST_BASE . "/forms";
+
+	$SITE_SHOP_BASE		=	$SITE_BASE . "/shop";
+	$SITE_SHOP_FORMS	=	$SITE_SHOP_BASE . "/forms";
 
 	// URLs
 
 	$SITE_BASE_URL 		= 	"http://" . $SITE_ADDR;
-	$SITE_INCLUDE_URL 	=	 $SITE_BASE_URL . "/include";
+	$SITE_CSS_URL 		=	$SITE_BASE_URL . "/css";
+	$SITE_JS_URL 		=	$SITE_BASE_URL . "/js";
+	$SITE_IMAGE_URL		=	$SITE_BASE_URL . "/images";
 
 	$SITE_ADMIN_BASE_URL		=	$SITE_BASE_URL . "/admin";
-	$SITE_ADMIN_INCLUDE_URL		=	$SITE_ADMIN_BASE_URL . "/include";
+	
+	$SITE_ARTIST_BASE_URL	=	$SITE_BASE_URL . "/artist";
+	$SITE_ARTIST_CSS_URL	=	$SITE_ARTIST_BASE_URL . "/css";
+	$SITE_ARTIST_JS_URL		=	$SITE_ARTIST_BASE_URL . "/js";
 
+	$SITE_SHOP_BASE_URL		=	$SITE_BASE_URL . "/shop";
+	$SITE_SHOP_CSS_URL		=	$SITE_SHOP_BASE_URL . "/css";
+	$SITE_SHOP_JS_URL		=	$SITE_SHOP_BASE_URL . "/js";
 
 	// db
 	$databaseName = "testDB";
