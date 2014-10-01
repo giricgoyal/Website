5d2717d4daad7106cd59674ea087122f3cdd8f4b
Giric Goyal <giricgoyal@gmail.com>
2014-10-01 10:31:51 -0500

    update

---
+++ artnartistsComingSoon/runserver.bat
@@ -0,0 +1,6 @@
+@echo off
+
+echo Running manager
+serverGUI.py
+
+pause
---
+++ artnartistsComingSoon/server/serverproject/server/envWin/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Github\Website\artnartistsComingSoon\server\serverproject\server\envWin\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
