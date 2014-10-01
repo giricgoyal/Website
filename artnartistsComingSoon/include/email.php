55b0d6999bf9cc3ca759890855a5a0459fb81fb2
Giric Goyal <giricgoyal@gmail.com>
2014-10-01 16:37:25 -0500

    updating email and editing text

--- artnartistsComingSoon/include/email.php
+++ artnartistsComingSoon/include/email.php
@@ -9,8 +9,7 @@
 					$message = "<html >
 									<body>
 										<img src='" . $logoimage . "/logo.png' alt='logo'/><br>
-										<span style='font-size:0.7em; color: red'>Paint it Red</span><br><br>
-										<span style='font-size: 2em; color: #9112a2'>Thank you for signing up, " . $name . "</span><br><br>
+										<span style='font-size: 2em; color: #101010'>Thank you for signing up, " . $name . "</span><br><br>
 										<span style='font-size: 1.2em'>We will be here soon. Till then, share this URL with atleast 3 of your friends to get early access to the wall of fame.</span><br><br>
 										<span style='text-align: center'>" . $url . "</span><br><br>
 										<span>Regards</span><br>
