22a7820c65e5ad1061ca9b1b525f843120120e6d
giricgoyal <giricgoyal@gmail.com>
2014-10-05 22:26:54 -0500

    update

--- artnartistsComingSoon/include/variables.php
+++ artnartistsComingSoon/include/variables.php
@@ -15,31 +15,19 @@
 	
 	$MAIL_TO = "artinartistofficial@gmail.com";
 
-	$SITE_IP = "localhost";
-	$SITE_ADDR = $SITE_IP;
-	
-	if ($_SERVER['SERVER_NAME'] != "localhost") {
-		$SITE_ADDR = $_SERVER['SERVER_NAME'];
-	}
+	$SITE_ADDR = $_SERVER['SERVER_NAME'];
 	
 	$DB_SERVER			= 	"http://127.0.0.1:8000";
 	$DB_SERVER_USER		=	$DB_SERVER . "/useraccount/";
 	
 	$SITE_BASE 			= 	realpath($_SERVER["DOCUMENT_ROOT"]);
 	$SITE_INCLUDE 		= 	$SITE_BASE . "/include";
-	$SITE_FORMS			=	$SITE_BASE . "/forms";
-
-		
+	$SITE_FORMS			=	$SITE_BASE . "/forms";	
 
 	// URLs
 	$SITE_BASE_URL 		= 	"http://" . $SITE_ADDR;
 	$SITE_CSS_URL 		=	$SITE_BASE_URL . "/css";
 	$SITE_JS_URL 		=	$SITE_BASE_URL . "/js";
 	$SITE_IMAGE_URL		=	$SITE_BASE_URL . "/images";
-
-
-	// db
-	$databaseName = "testDB";
-	$userTable = "testtable1";
 
 ?>
